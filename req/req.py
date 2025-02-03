32ca43dccfe77fd70192903a8ec69ead59e1eeb6
rif av <reef.abramson@gmail.com>
2025-02-03 15:51:28 +0200

    add rickroll protection to req

--- req/req.py
+++ req/req.py
@@ -7,6 +7,8 @@ res = requests.get(url)
 
 page = str(res.text)
 
-
-with open('req.html', 'w', encoding='utf-8') as file:
-        file.write(page)
+if 'never gonna' in page or 'rick roll' in page or 'rickroll' in page or 'rick' in page:
+    print('dont open its a rickroll')
+else:
+        with open('req.html', 'w', encoding='utf-8') as file:
+                file.write(page)
