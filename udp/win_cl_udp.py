5eb5c7544f612495f3d35849447d6e93e5a9d881
rif av <reef.abramson@gmail.com>
2025-04-29 16:43:46 +0300

    now cd is working

--- udp/win_cl_udp.py
+++ udp/win_cl_udp.py
@@ -8,12 +8,9 @@ server_addr = ('127.0.0.1', 1337)
 
 
 def rd_msg(soc):
-    msg, addr = soc.recvfrom(1024)
+    msg, addr = soc.recvfrom(65507)
     msg = json.loads(msg.decode())
-    if type(msg) == list:
-        return {'msg': ''.join(msg), 'addr': addr}
-    else:
-        return {'msg': msg, 'addr': addr}
+    return {'msg': msg, 'addr': addr}
     
 
 def wr_msg(soc, msg):
@@ -24,14 +21,17 @@ def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind(src_addr)
     wr_msg(s, 'cd')
-    path_loc = rd_msg(s)['msg']
+    path_loc = rd_msg(s)['msg']['cwd']
     while True:
         i = input(f'PS {path_loc.strip()}>')
         wr_msg(s, i)
-        if i == 'exit()':
+        if i == 'exit':
             break
-        msg = rd_msg(s)['msg']
-        print(f'{msg.strip()}')
+        msg: dict[str, str] = rd_msg(s)['msg']
+        if 'output' in msg:
+            print(f'{msg['output'].strip()}')
+        elif 'cwd' in msg:
+            path_loc = msg['cwd']
 
     s.close()
 
--- udp/win_cmd.py
+++ udp/win_cmd.py
@@ -1,67 +1,61 @@
 import json
 import socket
 import subprocess
+import os
 
 src_addr = ('127.0.0.1', 1337)
-
 
 def rd_msg(soc):
     msg, addr = soc.recvfrom(1024)
     msg = msg.decode()
     return {'msg': msg, 'addr': addr}
-    
 
-def wr_msg(soc, msg, add):
-    soc.sendto(msg.encode(), add)
-
-
-def cmd_split(cmd):
-    splt = cmd.split(' ')
-    if splt == [cmd]:
-        return splt
-    l = []
-    for i in range(1, len(splt)):
-        l.append(splt[i])
-    str2 = ''.join(l)
-    str1 = splt[0]
-    return [str1, str2]
-
+def wr_msg(soc, msg, addr):
+    soc.sendto(msg.encode(), addr)
 
 def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind(src_addr)
     print('Server is up')
+
+    current_dir = os.getcwd()
+
     while True:
         msg_addr = rd_msg(s)
-        msg = msg_addr['msg']
+        msg: str = msg_addr['msg']
         addr = msg_addr['addr']
-        splt_cmd = cmd_split(msg)
-        if msg == 'exit()':
+
+        if msg == 'exit':
             break
-        output = b''
-        err = b''
-        try:
-            output = subprocess.run(splt_cmd, shell=True, capture_output=True)
-            err = str(output.stderr.decode()).encode()
-            output = str(output.stdout.decode()).encode()
-            print(f'command: {msg}\n\n output: {output.decode()}')
-            if err != b'':
-                print(f'err: {err.decode()}')
-        except subprocess.CalledProcessError:
-            print(f'command {msg} is not found')
-            output = f'/bin/sh: 1: {msg}: not found\ncommand {msg} is not found\n\n'.encode()
-        except:
-            print('something else went wrong')
-            output = b'something else went wrong'
-        finally:
-            if err != b'':
-                msg = json.dumps(["err: '", err.decode(), "'"])
-                wr_msg(s, msg, addr)
+
+        if msg.startswith('cd'):
+            parts = msg.split(maxsplit=1)
+            if len(parts) == 2:
+                new_path = os.path.abspath(os.path.join(current_dir, parts[1]))
+                if os.path.isdir(new_path):
+                    current_dir = new_path
+                    wr_msg(s, json.dumps({"cwd": current_dir}), addr)
+                else:
+                    wr_msg(s, json.dumps({"output": f"cd : Cannot find path '{new_path}' because it does not exist."}), addr)
             else:
-                wr_msg(s, json.dumps(output.decode()), addr)
+                wr_msg(s, json.dumps({"cwd": current_dir}), addr)
+            continue
+
+        try:
+            result = subprocess.run(msg, shell=True, capture_output=True, cwd=current_dir)
+            output = result.stdout.decode()
+            error = result.stderr.decode()
+
+            if error:
+                wr_msg(s, json.dumps({"output": error}), addr)
+            else:
+                wr_msg(s, json.dumps({"output": output}), addr)
+
+        except Exception as e:
+            wr_msg(s, json.dumps({'output': f"Server error: {str(e)}"}), addr)
 
     s.close()
     print('Server is down')
- 
+
 if __name__ == '__main__':
     main()
