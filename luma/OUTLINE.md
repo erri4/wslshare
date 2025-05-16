1cd2bfd0a54549d1d251be452037ed3c763f8e9c
rif av <reef.abramson@gmail.com>
2025-05-16 23:52:54 +0300

    checkpoint (not working)

---
+++ luma/OUTLINE.md
@@ -0,0 +1,10 @@
+pip install / site : vexainstaller.exe
+which goes to erri4 / vexa-itself repo and gets executables and stuff
+file structure:
+C:/
+ - vexa/
+ - - bin/
+ - - - # this in PATH
+ - - - the executables
+ - - lib/
+ - - - the libraries
---
+++ luma/pmfl.py
@@ -0,0 +1,26 @@
+# pmfl stands for package manager for luma.
+# this is like PIP for python
+
+# OUTLINE:
+
+# pmfl get pkg
+# detect if pkg id a url and if it is do and http to pkg and install the zip result id there is else install pkg from my server on pmfl.pythonanywhere.com
+
+# pmfl remove pkg_name
+# uninstall pkg_name
+
+# pmfl show pkg_name
+# show data about pkg_name:
+# name
+# version
+# source (url / my server)
+
+# pmfl list
+# list installed packages
+
+# installing: put files in lib\third-party-pkgs
+
+# file type: .zip
+
+# pmfl get -r filename
+# works like pip install -r filename
