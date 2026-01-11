34160cc71524ad590ec6f6d3de42b3e8b7f531c6
rif av <reef.abramson@gmail.com>
2026-01-11 14:16:07 +0200

    encrypted wordle and connections and nytime for trolls

--- connwrdle.py
+++ connwrdle.py
@@ -1,15 +1,16 @@
 import datetime
 import requests
 
-date = str(datetime.date(datetime.datetime.now().year, datetime.datetime.now().month, datetime.datetime.now().day))
-wordle = "https://www.nytimes.com/svc/wordle/v2/" + date + ".json"
-connctions = "https://www.nytimes.com/svc/connections/v2/" + date + ".json"
+url = bytes.fromhex('7777772e6e7974696d65732e636f6d2f7376632f').decode()
+date = str(datetime.date(datetime.datetime.now().year, datetime.datetime.now().month, datetime.datetime.now().day + 1))
+w = f"https://www.nytimes.com/svc/{bytes.fromhex('574f52444c45').decode().lower()}/v2/" + date + ".json"
+c = f"https://www.nytimes.com/svc/{bytes.fromhex('434f4e4e454354494f4e53').decode().lower()}/v2/" + date + ".json"
 
-wreq = requests.get(wordle)
-print('WORDLE:')
+wreq = requests.get(w)
+print(bytes.fromhex('574f52444c453a').decode())
 print(wreq.json()['solution'])
-creq = requests.get(connctions)
-print('CONNECTIONS:')
+creq = requests.get(c)
+print(bytes.fromhex('434f4e4e454354494f4e533a').decode())
 for category in creq.json()['categories']:
     for card in category['cards']:
         print(card['content'], end=' ')
