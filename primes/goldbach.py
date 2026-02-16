8209ec28422a3c43aba585d2ca6299f96520cfb9
rif av <reef.abramson@gmail.com>
2026-02-16 15:00:29 +0200

    .

--- primes/goldbach.py
+++ primes/goldbach.py
@@ -3,14 +3,13 @@ primes = [2, 3]
 
 def isprime(num: int):
     if math.sqrt(num) == int(math.sqrt(num)): return False
-    for i in range(2, math.ceil(math.sqrt(num))):
+    for i in range(3, math.ceil(math.sqrt(num)), 2):
         if num % i == 0:
             return False
     return True
 
 mx = 100000
 found = []
-rng = range(0, mx, 2)
 for i in range(0, mx, 2):
     for j in primes:
         if isprime(i + j):
@@ -18,4 +17,4 @@ for i in range(0, mx, 2):
             found.append(i)
             break
 
-print(len(found) == len(rng))
+print(len(found) == mx / 2)
