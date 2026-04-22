5a39905218d7b71410c139ba6f40672007f4aa09
rif av <reef.abramson@gmail.com>
2026-04-22 18:03:48 +0300

    moved cupp to tools and polyglot2 made shorter

--- polyglot2.cpp
+++ polyglot2.cpp
@@ -1,14 +1,8 @@
-int
-__randomname = 1 // 1; '''
-;
-#include <iostream>
+#/*
+# python goes here
+'''*/
 int main(){
-    std::cout << "hello";
-    // cpp goes here
+    // c++ goes here
     return 0;
 }
-// '''
-#if 0
-print('hi')
-# python goes here
-#endif
+//'''
